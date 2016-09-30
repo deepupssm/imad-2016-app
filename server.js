7ce21de957de4b2ed65677467e7584f9238e965f
deepupssm <deepupyma@gmail.com>
2016-09-30 23:45:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/sound1', function (req , res){
+    res.send('this is the first artical');
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
